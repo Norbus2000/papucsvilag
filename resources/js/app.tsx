cb013711ccea5ee3e6e212b93489b75ec3db28cc
Norbus2000 <odornorbert.on@gmail.com>
2023-07-04 17:52:16 +0200

    first commit

---
+++ resources/js/app.tsx
@@ -0,0 +1,38 @@
+import './bootstrap';
+import '../css/app.css';
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { createInertiaApp } from '@inertiajs/react';
+import { RouteContext } from '@/Hooks/useRoute';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { LaravelReactI18nProvider } from 'laravel-react-i18n';
+
+const appName =
+  window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+
+createInertiaApp({
+  title: title => `${title} - ${appName}`,
+  progress: {
+    color: '#4B5563',
+  },
+  resolve: name =>
+    resolvePageComponent(
+      `./Pages/${name}.tsx`,
+      import.meta.glob('./Pages/**/*.tsx'),
+    ),
+  setup({ el, App, props }) {
+    const root = createRoot(el);
+    return root.render(
+      <RouteContext.Provider value={(window as any).route}>
+        <LaravelReactI18nProvider
+          locale={'hu'}
+          fallbackLocale={'hu'}
+          files={import.meta.glob('/lang/*.json')}
+        >
+          <App {...props} />
+        </LaravelReactI18nProvider>
+      </RouteContext.Provider>,
+    );
+  },
+});
